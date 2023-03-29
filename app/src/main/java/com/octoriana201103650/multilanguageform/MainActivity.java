1df83b3a605c2a494000a1e264113306889c2159
juliana <ceriap860@gmail.com>
2023-03-29 14:08:14 +0700

    ria

---
+++ app/src/main/java/com/octoriana201103650/multilanguageform/MainActivity.java
@@ -0,0 +1,13 @@
+package com.octoriana201103650.multilanguageform;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
